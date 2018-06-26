9facff925da6b74ea43531964e5caa89322d7801
Matt <matt.leonard.22@gmail.com>
2018-06-26 12:27:54 -0700

    Remove sensitive data and use env variables instead

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 const bodyParser = require('body-parser');
 const routes = require('./routes');
 
--- db/index.js
+++ db/index.js
@@ -1,11 +1,10 @@
 const { Pool } = require('pg');
-const {
-  user,
-  host,
-  database,
-  password,
-  port,
-} = require('../secrets/db_configuration');
+const user = process.env.DB_USER;
+const host = process.env.DB_HOST;
+const database = process.env.DB_DATABASE;
+const password = process.env.DB_PASSWORD;
+const port = process.env.DB_PORT;
+
 const pool = new Pool({ user, host, database, password, port });
 
 module.exports = pool;
