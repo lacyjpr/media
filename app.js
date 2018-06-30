726a81c25dbb6fd07b0623d9ef828fda1fd1b0f8
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-06-30 09:06:50 -0700

    Refactor out router

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 require('dotenv').config();
-const routes = require('./routes');
+const routes = require('./routes/');
 
 const app = express();
 
 // Middleware
 app.use(bodyParser.json());
-app.use('/', routes);
+//app.use('/', routes);
+require('./routes/users')(app);
 
 // Error handler
 app.use((err, req, res, next) => {
