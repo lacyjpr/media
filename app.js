36dd6d2b84191e81f9399e236b40dada7c25ca72
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-06-30 09:08:31 -0700

    Revert "Refactor out router"
    This reverts commit 726a81c25dbb6fd07b0623d9ef828fda1fd1b0f8.

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 require('dotenv').config();
-const routes = require('./routes/');
+const routes = require('./routes');
 
 const app = express();
 
 // Middleware
 app.use(bodyParser.json());
-//app.use('/', routes);
-require('./routes/users')(app);
+app.use('/', routes);
 
 // Error handler
 app.use((err, req, res, next) => {
