8d9f117a7cb6c50dd77b71bca235fd73e42b8a5f
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-07-08 10:28:26 -0700

    Add AuthContext.js

---
+++ client/src/AuthContext.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+const AuthContext = React.createContext();
+
+class AuthProvider extends Component {
+  constructor() {
+    super();
+
+    this.state = { isAuth: false };
+  }
+
+  render() {
+    return (
+      <AuthContext.Provider value={{ isAuth: this.state.isAuth }}>
+        {this.props.children}
+      </AuthContext.Provider>
+    );
+  }
+}
+
+const AuthConsumer = AuthContext.Consumer;
+
+export { AuthProvider, AuthConsumer };
