0db4a4ae6915d05937ceeb7b1ba5d1f1bb189b6f
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-07-08 10:32:07 -0700

    Add AuthProvider to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { AuthProvider } from './AuthContext';
 
 class App extends Component {
   componentDidMount() {
@@ -14,13 +15,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <AuthProvider>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </AuthProvider>
       </div>
     );
   }
