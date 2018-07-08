0217fe1fbc9a55bbee803b12f8d74bb2db51a43b
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-07-08 11:11:02 -0700

    Add Dashboard & Landing components

---
+++ client/src/Dashboard.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Dashboard = () => <h2>User Dashboard</h2>;
+
+export default Dashboard;
---
+++ client/src/Landing.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Landing = () => <h2>Landing Page</h2>;
+
+export default Landing;
