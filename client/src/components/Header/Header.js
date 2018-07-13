7d8978b5abc3c816a74142252d156ab975bf7a9c
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-07-13 09:14:11 -0700

    Get example code working

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,27 +1,87 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from 'reactstrap';
+
 import { AuthConsumer } from './../AuthContext';
 
-const Header = () => (
-  <header>
-    <AuthConsumer>
-      {({ isAuth, login, logout }) => (
-        <div>
-          <h3>
-            <Link to="/">HOME</Link>
-          </h3>
-          {isAuth ? (
-            <ul>
-              <Link to="/userhome">User Home</Link>
-              <button onClick={logout}>logout</button>
-            </ul>
-          ) : (
-            <button onClick={login}>login</button>
+class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isOpen: false,
+    };
+  }
+
+  toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  };
+
+  render() {
+    return (
+      <header>
+        <AuthConsumer>
+          {({ isAuth, login, logout }) => (
+            <div>
+              <h3>
+                <Link to="/">HOME</Link>
+              </h3>
+              {isAuth ? (
+                <ul>
+                  <Link to="/userhome">User Home</Link>
+                  <button onClick={logout}>logout</button>
+                </ul>
+              ) : (
+                <button onClick={login}>login</button>
+              )}
+            </div>
           )}
+        </AuthConsumer>
+        <div>
+          <Navbar color="light" light expand="md">
+            <NavbarBrand href="/">reactstrap</NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <NavLink href="/components/">Components</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="https://github.com/reactstrap/reactstrap">
+                    GitHub
+                  </NavLink>
+                </NavItem>
+                <UncontrolledDropdown nav inNavbar>
+                  <DropdownToggle nav caret>
+                    Options
+                  </DropdownToggle>
+                  <DropdownMenu right>
+                    <DropdownItem>Option 1</DropdownItem>
+                    <DropdownItem>Option 2</DropdownItem>
+                    <DropdownItem divider />
+                    <DropdownItem>Reset</DropdownItem>
+                  </DropdownMenu>
+                </UncontrolledDropdown>
+              </Nav>
+            </Collapse>
+          </Navbar>
         </div>
-      )}
-    </AuthConsumer>
-  </header>
-);
+      </header>
+    );
+  }
+}
 
 export default Header;
