9290710ea4060487d10dbdc0ce9b1f038a9e9f93
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-07-15 13:29:19 -0700

    Combine AuthConsumers

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -23,7 +23,7 @@ class Header extends Component {
           <Navbar color="light" light expand="xs">
             <NavbarBrand href="/">Media</NavbarBrand>
             <Nav className="ml-auto" navbar>
-              <AuthConsumer>
+              {/* <AuthConsumer>
                 {({ isAuth, login }) => (
                   <NavItem>
                     {!isAuth && (
@@ -33,31 +33,40 @@ class Header extends Component {
                     )}
                   </NavItem>
                 )}
-              </AuthConsumer>
+              </AuthConsumer> */}
 
               <AuthConsumer>
-                {({ isAuth, logout }) => (
-                  <UncontrolledDropdown nav inNavbar>
-                    {isAuth && (
-                      <div>
-                        <DropdownToggle nav caret>
-                          <img src={person} alt="person" />
-                        </DropdownToggle>
-                        <DropdownMenu right>
-                          <DropdownItem>
-                            <NavLink tag={Link} to="/userhome">
-                              User Home
-                            </NavLink>
-                          </DropdownItem>
-                          <DropdownItem>
-                            <NavLink href="#" onClick={logout}>
-                              logout
-                            </NavLink>
-                          </DropdownItem>
-                        </DropdownMenu>
-                      </div>
-                    )}
-                  </UncontrolledDropdown>
+                {({ isAuth, login, logout }) => (
+                  <div>
+                    <NavItem>
+                      {!isAuth && (
+                        <NavLink href="#" onClick={login}>
+                          login
+                        </NavLink>
+                      )}
+                    </NavItem>
+                    <UncontrolledDropdown nav inNavbar>
+                      {isAuth && (
+                        <div>
+                          <DropdownToggle nav caret>
+                            <img src={person} alt="person" />
+                          </DropdownToggle>
+                          <DropdownMenu right>
+                            <DropdownItem>
+                              <NavLink tag={Link} to="/userhome">
+                                User Home
+                              </NavLink>
+                            </DropdownItem>
+                            <DropdownItem>
+                              <NavLink href="#" onClick={logout}>
+                                logout
+                              </NavLink>
+                            </DropdownItem>
+                          </DropdownMenu>
+                        </div>
+                      )}
+                    </UncontrolledDropdown>
+                  </div>
                 )}
               </AuthConsumer>
             </Nav>
