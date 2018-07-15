835b76c6e3c18e73baaa0080ce3b89975ca33d54
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-07-15 15:35:14 -0700

    Remove class

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import {
   Navbar,
@@ -16,52 +16,50 @@ import person from '../../assets/person-outline.svg';
 import { AuthConsumer } from './../AuthContext';
 import './Header.css';
 
-class Header extends Component {
-  render() {
-    return (
-      <header>
-        <Navbar color="light" light expand="xs">
-          <NavbarBrand href="/">Media</NavbarBrand>
-          <Nav className="ml-auto" navbar>
-            <AuthConsumer>
-              {({ isAuth, login, logout }) => (
-                <div>
-                  <NavItem>
-                    {!isAuth && (
-                      <NavLink href="#" onClick={login}>
-                        login
-                      </NavLink>
-                    )}
-                  </NavItem>
-                  <UncontrolledDropdown nav inNavbar>
-                    {isAuth && (
-                      <div>
-                        <DropdownToggle nav caret>
-                          <img src={person} alt="person" className="person" />
-                        </DropdownToggle>
-                        <DropdownMenu right>
-                          <DropdownItem>
-                            <NavLink tag={Link} to="/userhome">
-                              User Home
-                            </NavLink>
-                          </DropdownItem>
-                          <DropdownItem>
-                            <NavLink href="#" onClick={logout}>
-                              logout
-                            </NavLink>
-                          </DropdownItem>
-                        </DropdownMenu>
-                      </div>
-                    )}
-                  </UncontrolledDropdown>
-                </div>
-              )}
-            </AuthConsumer>
-          </Nav>
-        </Navbar>
-      </header>
-    );
-  }
-}
+const Header = () => {
+  return (
+    <header>
+      <Navbar color="light" light expand="xs">
+        <NavbarBrand href="/">Media</NavbarBrand>
+        <Nav className="ml-auto" navbar>
+          <AuthConsumer>
+            {({ isAuth, login, logout }) => (
+              <div>
+                <NavItem>
+                  {!isAuth && (
+                    <NavLink href="#" onClick={login}>
+                      login
+                    </NavLink>
+                  )}
+                </NavItem>
+                <UncontrolledDropdown nav inNavbar>
+                  {isAuth && (
+                    <div>
+                      <DropdownToggle nav caret>
+                        <img src={person} alt="person" className="person" />
+                      </DropdownToggle>
+                      <DropdownMenu right>
+                        <DropdownItem>
+                          <NavLink tag={Link} to="/userhome">
+                            User Home
+                          </NavLink>
+                        </DropdownItem>
+                        <DropdownItem>
+                          <NavLink href="#" onClick={logout}>
+                            logout
+                          </NavLink>
+                        </DropdownItem>
+                      </DropdownMenu>
+                    </div>
+                  )}
+                </UncontrolledDropdown>
+              </div>
+            )}
+          </AuthConsumer>
+        </Nav>
+      </Navbar>
+    </header>
+  );
+};
 
 export default Header;
