4c1806cde01b79901869b218c1c654b2627d37e3
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-07-15 13:31:32 -0700

    Remove excess div

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -19,47 +19,45 @@ class Header extends Component {
   render() {
     return (
       <header>
-        <div>
-          <Navbar color="light" light expand="xs">
-            <NavbarBrand href="/">Media</NavbarBrand>
-            <Nav className="ml-auto" navbar>
-              <AuthConsumer>
-                {({ isAuth, login, logout }) => (
-                  <div>
-                    <NavItem>
-                      {!isAuth && (
-                        <NavLink href="#" onClick={login}>
-                          login
-                        </NavLink>
-                      )}
-                    </NavItem>
-                    <UncontrolledDropdown nav inNavbar>
-                      {isAuth && (
-                        <div>
-                          <DropdownToggle nav caret>
-                            <img src={person} alt="person" />
-                          </DropdownToggle>
-                          <DropdownMenu right>
-                            <DropdownItem>
-                              <NavLink tag={Link} to="/userhome">
-                                User Home
-                              </NavLink>
-                            </DropdownItem>
-                            <DropdownItem>
-                              <NavLink href="#" onClick={logout}>
-                                logout
-                              </NavLink>
-                            </DropdownItem>
-                          </DropdownMenu>
-                        </div>
-                      )}
-                    </UncontrolledDropdown>
-                  </div>
-                )}
-              </AuthConsumer>
-            </Nav>
-          </Navbar>
-        </div>
+        <Navbar color="light" light expand="xs">
+          <NavbarBrand href="/">Media</NavbarBrand>
+          <Nav className="ml-auto" navbar>
+            <AuthConsumer>
+              {({ isAuth, login, logout }) => (
+                <div>
+                  <NavItem>
+                    {!isAuth && (
+                      <NavLink href="#" onClick={login}>
+                        login
+                      </NavLink>
+                    )}
+                  </NavItem>
+                  <UncontrolledDropdown nav inNavbar>
+                    {isAuth && (
+                      <div>
+                        <DropdownToggle nav caret>
+                          <img src={person} alt="person" />
+                        </DropdownToggle>
+                        <DropdownMenu right>
+                          <DropdownItem>
+                            <NavLink tag={Link} to="/userhome">
+                              User Home
+                            </NavLink>
+                          </DropdownItem>
+                          <DropdownItem>
+                            <NavLink href="#" onClick={logout}>
+                              logout
+                            </NavLink>
+                          </DropdownItem>
+                        </DropdownMenu>
+                      </div>
+                    )}
+                  </UncontrolledDropdown>
+                </div>
+              )}
+            </AuthConsumer>
+          </Nav>
+        </Navbar>
       </header>
     );
   }
