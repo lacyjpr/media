1e2cb8981e0489a12717436c24de7dd85813a40c
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-07-23 11:26:19 -0700

    Remove boilerplate

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import SlateEditor from './components/SlateEditor';
 
 import { Button } from 'reactstrap';
 
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -18,13 +18,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
+        </header> */}
+        {/* <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
         <SlateEditor />
         <Button color="primary">Bootstrap Button</Button>
       </div>
