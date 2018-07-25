180b65520a4e5e6da8343c9b336f2a2afeb5f055
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-07-25 10:02:46 -0700

    Add renderNode & renderMark to viewer

--- client/src/components/SlateViewer/SlateViewer.js
+++ client/src/components/SlateViewer/SlateViewer.js
@@ -8,11 +8,65 @@ class SlateViewer extends Component {
   state = {
     value: Value.fromJSON(existingValue),
   };
+
+  /**
+   * Render a Slate node.
+   *
+   * @param {Object} props
+   * @return {Element}
+   */
+
+  renderNode = props => {
+    const { attributes, children, node } = props;
+
+    switch (node.type) {
+      case 'block-quote':
+        return <blockquote {...attributes}>{children}</blockquote>;
+      case 'bulleted-list':
+        return <ul {...attributes}>{children}</ul>;
+      case 'heading-one':
+        return <h1 {...attributes}>{children}</h1>;
+      case 'heading-two':
+        return <h2 {...attributes}>{children}</h2>;
+      case 'list-item':
+        return <li {...attributes}>{children}</li>;
+      case 'numbered-list':
+        return <ol {...attributes}>{children}</ol>;
+    }
+  };
+
+  /**
+   * Render a Slate mark.
+   *
+   * @param {Object} props
+   * @return {Element}
+   */
+
+  renderMark = props => {
+    const { children, mark, attributes } = props;
+
+    switch (mark.type) {
+      case 'bold':
+        return <strong {...attributes}>{children}</strong>;
+      case 'code':
+        return <code {...attributes}>{children}</code>;
+      case 'italic':
+        return <em {...attributes}>{children}</em>;
+      case 'underlined':
+        return <u {...attributes}>{children}</u>;
+    }
+  };
+
   render() {
     return (
       <div>
         <h2>Slate Viewer</h2>
-        <Editor readOnly value={this.state.value} />
+        <Editor
+          readOnly
+          value={this.state.value}
+          renderNode={this.renderNode}
+          renderMark={this.renderMark}
+        />
       </div>
     );
   }
