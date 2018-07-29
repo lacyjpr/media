3272f8b1c3392d1c66edc4eb618ac08602bff786
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-07-29 11:12:50 -0700

    Get title from local storage

--- client/src/components/SlateViewer/SlateViewer.js
+++ client/src/components/SlateViewer/SlateViewer.js
@@ -4,9 +4,10 @@ import { Value } from 'slate';
 import { renderNode, renderMark } from '../../Utilities';
 
 const existingValue = JSON.parse(localStorage.getItem('content'));
-
+const existingTitle = localStorage.getItem('title');
 class SlateViewer extends Component {
   state = {
+    title: existingTitle || 'Title',
     value: Value.fromJSON(existingValue),
   };
 
@@ -14,6 +15,7 @@ class SlateViewer extends Component {
     return (
       <div className="wrap-editor">
         <h2>Slate Viewer</h2>
+        <h1>{this.state.title}</h1>
         <Editor
           readOnly
           value={this.state.value}
