6a7d2684beb811537ebdad7d78563e405ecd1e7a
Jean-Paul Lacy <lacyjpr@gmail.com>
2019-07-28 10:19:43 -0700

    Remove timeout on login

--- client/src/components/AuthContext.js
+++ client/src/components/AuthContext.js
@@ -13,7 +13,9 @@ class AuthProvider extends Component {
   login = () => {
     // Fake async with setTimeout
     console.log('login called');
-    setTimeout(() => this.setState({ isAuth: true }), 1000);
+    // setTimeout(() =>
+     this.setState({ isAuth: true });
+    //  , 1000);
     console.log(this.state.isAuth);
   };
 
